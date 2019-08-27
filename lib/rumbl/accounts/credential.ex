f5712972a1f8ff1f03fca89bdf9a41f759ec8ce3
Peter Melinat <github@mailin.at>
2019-08-27 10:06:46 +0200

    Authenticating Users (chapter 5)

---
+++ lib/rumbl/accounts/credential.ex
@@ -0,0 +1,30 @@
+defmodule Rumbl.Accounts.Credential do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "credentials" do
+    field :email, :string
+    field :password,:string, virtual: true
+    field :password_hash, :string
+    belongs_to :user, Rumbl.Accounts.User
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(credential, attrs) do
+    credential
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> unique_constraint(:email)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(password))
+  end
+  defp put_pass_hash(changeset), do: changeset 
+
+end
+
--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,12 +1,15 @@
 defmodule RumblWeb.UserController do
+  import Logger
 	use RumblWeb, :controller
 
+  alias Elixir.Plug.Conn
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
+  plug :authenticate when action in [:index, :show]
+
   def index(conn, _params) do
-    users =Accounts.list_users()
-    render(conn, "index.html", users: users)
+    render(conn, "index.html", users: Accounts.list_users())
   end
 
   def show(conn, %{"id" => id}) do
@@ -14,17 +17,29 @@ defmodule RumblWeb.UserController do
     render(conn, "show.html", user: user)
   end
   def new(conn, _params) do
-    changeset = Accounts.change_user(%User{})
+    changeset = Accounts.change_registration(%User{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
+    case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
+        |> RumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: Routes.user_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
+  end
+
+
+  defp authenticate(%Conn{ assigns: %{current_user: %{username: user}}} = conn, _) when is_binary(user) do
+    conn
+  end
+  defp authenticate(conn, _) do
+    conn
+    |> put_flash(:error, "You must be logged in to access that page")
+    |> redirect(to: Routes.page_path(conn, :index))
+    |> halt()
   end
 end
